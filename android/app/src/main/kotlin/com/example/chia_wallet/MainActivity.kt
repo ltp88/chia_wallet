6080c36806fa20828618230b75ec343a7495a41f
Phuong Lam <lamtungphuongtv@gmail.com>
2021-05-23 13:02:37 -0400

    Move project

--- android/app/src/main/kotlin/com/example/chia_wallet/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.chia_wallet
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ ios/soa/soa.metal
@@ -0,0 +1,11 @@
+//
+//  soa.metal
+//  soa
+//
+//  Created by Phuong Lam on 5/14/21.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+
